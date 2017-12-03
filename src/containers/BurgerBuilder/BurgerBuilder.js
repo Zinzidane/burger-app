faea2913137b3e7123ab827b2c1a058795cfb84a
ivan muliarchyk <ya-gans@mail.ru>
2017-12-03 21:23:51 +0300

    Ouput burger ingredients dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,28 @@
+import BurgerIngredient from '../../components/Burger/BurgerIngredient/BurgerIngredient';
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
+
   render() {
+    const transformedIngredients = Object.keys(props.ingredients)
+      .map(ingKey => {
+        return [...Array(props.ingredients.[ingKey])].map((_, i) => {
+          return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+        });
+      }); // it gives us an array of keys. We use Array() to get ingredients which quantity more than 1
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
