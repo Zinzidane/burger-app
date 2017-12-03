8eb671d26ee413a1bd11cc70999cd9ccadf2e9ad
ivan muliarchyk <ya-gans@mail.ru>
2017-12-03 21:30:02 +0300

    Fix burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    }); // it gives us an array of keys. We use Array() to get ingredients which quantity more than 1
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="cheese" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,3 @@
-import BurgerIngredient from '../../components/Burger/BurgerIngredient/BurgerIngredient';
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
@@ -14,12 +13,6 @@ class BurgerBuilder extends Component {
   }
 
   render() {
-    const transformedIngredients = Object.keys(props.ingredients)
-      .map(ingKey => {
-        return [...Array(props.ingredients.[ingKey])].map((_, i) => {
-          return <BurgerIngredient key={ingKey + i} type={ingKey} />;
-        });
-      }); // it gives us an array of keys. We use Array() to get ingredients which quantity more than 1
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
