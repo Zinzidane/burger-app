9a364791ac315dad51673d366f64d515ee0e3b37
ivan muliarchyk <ya-gans@mail.ru>
2017-12-06 15:09:03 +0300

    Add basic routing

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -12,10 +12,10 @@ const checkoutSummary = (props) => {
       </div>
       <Button 
         btnType='Danger'
-        cliked>CANCEL</Button>
+        clicked={props.checkoutCancelled}>CANCEL</Button>
       <Button 
         btnType='Success'
-        clicked>CONTINUE</Button>
+        clicked={props.checkoutContinued}>CONTINUE</Button>
     </div>
   ); 
 };
