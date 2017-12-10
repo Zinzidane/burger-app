262d23aab55b3508b29ab455c93a5c25d434e953
ivan muliarchyk <ya-gans@mail.ru>
2017-12-10 14:28:42 +0300

    Add signin

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     };
-    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBn6f4g4lr6x62AjNOo3EXVCNSV9P78muQ', authData)
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBn6f4g4lr6x62AjNOo3EXVCNSV9P78muQ';
+    if(!isSignup) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBn6f4g4lr6x62AjNOo3EXVCNSV9P78muQ';
+    }
+    axios.post(url, authData)
       .then(res => {
         console.log(res);
         dispatch(authSuccess(res.data));
