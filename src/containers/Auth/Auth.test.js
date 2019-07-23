c6c0092a0019132f34be98a1159280a8280e6826
imuliarchyk <imuliarchyk@orion.intra>
2019-07-23 11:11:24 +0300

    update tests

--- src/containers/Auth/Auth.test.js
+++ src/containers/Auth/Auth.test.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import Auth from './Auth';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import configureStore from 'redux-mock-store'
+import sinon from 'sinon';
+import configureStore from 'redux-mock-store';
 
 configure({ adapter: new Adapter() });
 
 describe('<Auth />', () => {
+    const mockStore = configureStore([thunk]);
     const initialState = {
         loading: false,
         error: null,
-        isAuthenticated: false
+        isAuthenticated: false,
+        buildingBurger: false,
+        authRedirectPath: '/'
     }; 
-    const mockStore = configureStore();
-    const mockLoadingfn = jest.fn();
     let wrapper;
     let store;
-    // const props = {
-    //     loading: false
-    // };
+
     beforeEach(() => {
-      store = mockStore(initialState);
-      wrapper = shallow(<Auth store={store} />);
+
+        store = mockStore(initialState);
+
+        wrapper = shallow(
+            <Provider store={store}><Auth {...initialState} /></Provider> 
+        );
     });
 
     it('renders correctly', () => {
+        // expect(wrapper.props().loading).toBe(initialState.loading);
         expect(wrapper).toMatchSnapshot();
     });
     
+    it('should have correct props', () => {
+        expect(wrapper.props()).toEqual(initialState);
+    });
 
 });
